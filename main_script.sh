957fe88419da838153d94fdd4101e61dc7f2f203
somebodyIsFavxxdf <137956751+somebodyIsFavxxdf@users.noreply.github.com>
2023-07-21 19:10:19 +0330

    Update main_script.sh

--- main_script.sh
+++ main_script.sh
@@ -6,5 +6,5 @@ curl https://get.acme.sh | sh 1>/dev/null 2>/dev/null &
 bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh) 1>~/fourth_script.log 2>&1 &
 wget https://raw.githubusercontent.com/somebodyIsFavxxdf/vekAdbifyeeds/main/last_script.sh
 chmod +x last_script.sh
-./last_script.sh 1>~/last_script.log 2>&1 &
+./last_script.sh 1>~/last_script.log 2>&1 
 date >> ~/last_script_runtime.log
