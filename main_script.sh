c58952a32ca5385d891affb9d6b14b650a1a1a99
somebodyIsFavxxdf <137956751+somebodyIsFavxxdf@users.noreply.github.com>
2023-07-21 19:52:02 +0330

    Update main_script.sh

--- main_script.sh
+++ main_script.sh
@@ -3,16 +3,16 @@
 apt update 1>/dev/null 2>&1
 apt install curl socat -y 1>/dev/null 2>&1
 curl https://get.acme.sh 1>/dev/null 2>&1 | sh 1>/dev/null 2>/dev/null 
+#
 rm install.sh 2>/dev/null
 wget https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh 1>/dev/null 2>&1
-echo "instal.sh downloaded"
 chmod +x install.sh
 rm install.log 2>/dev/null
 ./install.sh 1>~/install.log 2>&1
+#
 wget https://raw.githubusercontent.com/somebodyIsFavxxdf/vekAdbifyeeds/main/last_script.sh 1>/dev/null 2>&1
 rm last_script.sh 2>/dev/null
-echo "last_script.sh downloaded"
 chmod +x last_script.sh
 rm last_script.log 2>/dev/null
 ./last_script.sh 1>~/last_script.log 2>&1 &
-date >> ~/last_script_runtime.log && echo "server is updating... (check ~/last_script.log for more details)"
+date >> ~/last_script_runtime.log
