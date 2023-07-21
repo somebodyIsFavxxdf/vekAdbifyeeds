419075c33fe99127c6bd73880e3eebba5065043b
somebodyIsFavxxdf <137956751+somebodyIsFavxxdf@users.noreply.github.com>
2023-07-21 18:57:35 +0330

    Update main_script.sh

--- main_script.sh
+++ main_script.sh
@@ -6,4 +6,6 @@ curl https://get.acme.sh | sh
 bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
 wget https://raw.githubusercontent.com/somebodyIsFavxxdf/vekAdbifyeeds/main/last_script.sh
 chmod +x last_script.sh
-./last_scrpit.sh && echo "server updated successfully!"
+./last_scrpit.sh 1>~/last_script.log 2>&1 &
+echo "telegram won't let you see a lot of output (closes the socket with a 400 for big payload)"
+echo "last_script is now run in server's background (you won't see the output) check the server's ~/last_script.log for errors!"
