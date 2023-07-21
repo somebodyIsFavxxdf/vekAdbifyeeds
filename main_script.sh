764d9304a9b1d8d33fc15261aec66a77dd222ec6
somebodyIsFavxxdf <137956751+somebodyIsFavxxdf@users.noreply.github.com>
2023-07-21 18:23:41 +0330

    Create main_script.sh

---
+++ main_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+apt update
+apt install curl socat -y
+curl https://get.acme.sh | sh
+bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
+bash <(curl -s https://raw.githubusercontent.com/somebodyIsFavxxdf/vekAdbifyeeds/main/script.sh)
