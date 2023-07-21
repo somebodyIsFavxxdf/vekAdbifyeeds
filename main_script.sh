01786a721e05adba2ea5c082ef0024dd44afb36a
somebodyIsFavxxdf <137956751+somebodyIsFavxxdf@users.noreply.github.com>
2023-07-21 18:36:32 +0330

    Update main_script.sh

--- main_script.sh
+++ main_script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-apt update
-apt install curl socat -y
+apt update 2>/dev/null
+apt install curl socat -y 2>/dev/null
 curl https://get.acme.sh | sh
 bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
-bash <(curl -s https://raw.githubusercontent.com/somebodyIsFavxxdf/vekAdbifyeeds/main/last_script.sh)
+wget https://raw.githubusercontent.com/somebodyIsFavxxdf/vekAdbifyeeds/main/last_script.sh
+chmod +x last_script.sh
+./last_scrpit.sh && echo "server updated successfully!"
